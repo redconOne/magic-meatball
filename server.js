e97093edce0def7e8a1d1abf3d76bc2b3bdf0b14
Ming Ng <69211773+redconOne@users.noreply.github.com>
2022-05-26 18:32:21 -0600

    initial upload

---
+++ server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const querystring = require('querystring');
+
+const server = http.createServer((req, res) => {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+
+  const readWrite = (file, contentType) => {
+    fs.readFile(file, function (err, data) {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.write(data);
+      res.end();
+    });
+  };
+  switch (page) {
+    case '/':
+      readWrite('index.html', 'text/html');
+      break;
+    case '/otherpage':
+      readWrite('otherpage.html', 'text/html');
+      break;
+    case '/otherotherpage':
+      readWrite('otherotherpage.html', 'text/html');
+      break;
+    case '/api':
+      let magicResponse = [
+        'It is certain',
+        'It is decidedly so',
+        'Reply hazy try again',
+        'Cannot predict now',
+        'Do not count on it',
+        'My sources say no',
+        'Outlook not so good',
+        'Signs point to yes',
+      ];
+      let flipResult = Math.floor(Math.random() * magicResponse.length);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      const objToJson = {
+        result: magicResponse[flipResult],
+      };
+      res.end(JSON.stringify(objToJson));
+      break;
+    case '/css/style.css':
+      fs.readFile('css/style.css', function (err, data) {
+        res.write(data);
+        res.end();
+      });
+      break;
+    case '/js/main.js':
+      readWrite('js/main.js', 'text/javascript');
+      break;
+  }
+});
+
+server.listen(8000);
